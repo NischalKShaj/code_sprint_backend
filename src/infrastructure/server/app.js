2e01be52993a1bc2188859810aed18c043a6062f
nischal <nischalkshaj5@gmail.com>
2024-08-05 20:53:37 +0530

    minor update

---
+++ src/infrastructure/server/app.js
@@ -0,0 +1,70 @@
+// file for starting the server
+// importing the packages
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("../database/connect");
+const cors = require("cors");
+const userRouter = require("../routes/user/userRoutes");
+const adminRouter = require("../routes/admin/adminRoutes");
+const messageRouter = require("../routes/messages/messageRoute");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+const http = require("http");
+const { Server } = require("socket.io");
+const chatService = require("../services/chatService");
+const cronJob = require("../services/dailyProblemService");
+const passport = require("../services/OauthService");
+const Session = require("express-session");
+dotenv.config();
+
+// configuring the app
+const app = express();
+const server = http.createServer(app);
+chatService.init(server);
+
+// configuring the scheduler
+cronJob();
+
+// setting the port for the server
+const port = process.env.PORT || 4000;
+
+// Serve static files from the uploads directory
+app.use("/uploads", express.static("src/infrastructure/storage/uploads"));
+
+// configuring the cookie-parser
+app.use(cookieParser());
+
+app.use(
+  Session({
+    secret: "your_secret_key",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false },
+  })
+);
+
+// enabling cors policy for the application
+app.use(
+  cors({
+    origin: "https://www.codesprint.live",
+    methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// for passing the data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// setting the routes
+app.use("/admin", adminRouter);
+app.use("/message", messageRouter);
+app.use("/", userRouter);
+
+// starting the server
+server.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
---
+++ src/infrastructure/services/OauthService.js
@@ -0,0 +1,31 @@
+// Import required modules and setup passport
+const passport = require("passport");
+const { Strategy: GoogleStrategy } = require("passport-google-oauth20");
+const dotenv = require("dotenv");
+dotenv.config();
+console.log("clientId", process.env.GOOGLE_CLIENT_ID);
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+      callbackURL: "http://localhost:4000/auth/google/callback",
+      scope: ["profile", "email"],
+    },
+    async function (accessToken, refreshToken, profile, done) {
+      return done(null, profile);
+    }
+  )
+);
+
+// Serialize and deserialize user functions
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+module.exports = passport;
