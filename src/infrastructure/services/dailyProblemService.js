c7fd0144679e74402773be8b0a5b6474caa3fe6c
nischal <nischalkshaj5@gmail.com>
2024-08-16 23:29:26 +0530

    removed all the console logs and unwanted comments

--- src/infrastructure/services/dailyProblemService.js
+++ src/infrastructure/services/dailyProblemService.js
@@ -8,9 +8,8 @@ const problemScheduler = require("../../application/usecase/problemUseCase/probl
 module.exports = () => {
   cron.schedule("30 2 * * *", async () => {
     try {
-      console.log("Running daily problem task at 9:30 AM");
+      console.log("Running daily problem task at 2:30 AM IST");
       const result = await problemScheduler.dailyProblem();
-      console.log("Daily problem task result:", result);
     } catch (error) {
       console.error("Error running daily problem task:", error);
     }
